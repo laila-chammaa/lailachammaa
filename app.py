5b8a44d19e5b1186f0cbf85fdcaa44e0feca06d9
Laila Chammaa <laylachammaa@gmail.com>
2020-09-19 14:38:25 -0400

    fixed about me page

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ def logout():
 
 if __name__ == '__main__':
     from os import environ
-    app.run(host='0.0.0.0', port=environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
